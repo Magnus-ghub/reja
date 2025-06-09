e30b64421e366ca6f4381e7456b325fc596f7bfd
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-06-09 18:47:16 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,32 @@
+console.log('Web serverni boshlash');
+const express = require("express");
+const app = express();
+const http = require('http');
+
+
+// 1: Kirish kodlari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// 2:  Session
+// 3: Views kodlar
+app.set("views",  "views");
+app.set("view engine", "ejs");
+
+
+// 4 Routing kodlar
+app.get("/hello", function (req, res) {
+    res.end("<h1>HELLO WORLD</h1>");
+});
+app.get("/gift", function (req, res) {
+    res.end("<h1>Siz sovg'alar sahifasidasi!</h1>");
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running on port: ${PORT}`);
+});
