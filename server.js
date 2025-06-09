20a857f63272c26318306637f4087eb607b3bc53
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-06-09 21:54:47 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log('Web serverni boshlash');
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require('http');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 
 // 1: Kirish kodlari
@@ -17,13 +28,17 @@ app.set("view engine", "ejs");
 
 
 // 4 Routing kodlar
-app.get("/hello", function (req, res) {
-    res.end("<h1>HELLO WORLD</h1>");
-});
-app.get("/gift", function (req, res) {
-    res.end("<h1>Siz sovg'alar sahifasidasi!</h1>");
+app.post("/create-item", (req, res) => {
+    // TODO: code db with here
 });
 
+app.get('/author', (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
