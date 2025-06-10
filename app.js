93f51a8283f50dc2e0cff4c1d237dd9b56f677bc
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-06-10 22:52:01 +0900

    feat: mongoDB ulanish

--- app.js
+++ app.js
@@ -15,15 +15,12 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 });
 
 // MongoDB chaqirish
-const db = require("./server").db();  //qaytar
-// const db = require("./server"); //chatgpt
-
+const db = require("./server").db();
 
 // 1: Kirish kodlari
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 
 // 2:  Session
 // 3: Views kodlar
@@ -33,36 +30,42 @@ app.set("view engine", "ejs");
 
 // 4 Routing kodlar
 app.post("/create-item", (req, res) => {
-    console.log("user entered /create-item");
+    // console.log("user entered /create-item");
     // console.log(req.body);
-    const new_reja = req.body.reja;
-    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.end("succeesfully added");
-        }
-    });
+    // const new_reja = req.body.reja;
+    // db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    //     if(err) {
+    //         console.log(err);
+    //         res.end("something went wrong");
+    //     } else {
+    //         res.end("succeesfully added");
+    //     }
+    // });
 });
 
 app.get('/author', (req, res) => {
     res.render("author", { user: user });
 });
 
+// app.get("/", function (req, res) {
+//     res.render("reja");
+// });
+
+
 app.get("/", function (req, res) {
-    console.log('user entered /');
-    db.collection("plans")
-     .find()
-     .toArray((err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-             console.log(data);
-             res.render("reja", { items: data });
-        }
-     });
+    res.render("reja");
+    // console.log('user entered /');
+    // db.collection("plans")
+    //  .find()
+    //  .toArray((err, data) => {
+    //     if(err) {
+    //         console.log(err);
+    //         res.end("something went wrong");
+    //     } else {
+    //          console.log(data);
+    //          res.render("reja", { items: data });
+    //     }
+    //  });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,28 +2,29 @@ const http = require('http');
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://magnus:zhvK8MSqc5mukvlL@cluster0.h2xiuv9.mongodb.net/";
+const connectionString = 
+"mongodb+srv://magnus:7HkHasiQHs8HwESn@cluster0.h2xiuv9.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0"
 
-mongodb.connect(connectionString, {
+
+mongodb.connect(
+    connectionString,
+ {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, (err, client) => {
+}, 
+    (err, client) => {
     if(err) console.log("ERROR on connection to MongoDB");
     else {
         console.log("MongoDB connection succeed");
-
-        module.exports = client;  // qaytar
-        // db = client.db();      //chatgpt
-        // module.exports = db;   //chatgpt
-
-
-
+        module.exports = client;
 
         const app = require("./app");
         const server = http.createServer(app);
         let PORT = 3000;
         server.listen(PORT, function() {
-        console.log(`The server is running on port: ${PORT}, http://localhost:${PORT}`);
+        console.log(
+            `The server is running on port: ${PORT}, http://localhost:${PORT}`
+        );
         });
      }
 });
