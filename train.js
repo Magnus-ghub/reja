1ab12fcd8a2b9e507f59af986dfda6a3de95660f
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-06-14 21:06:37 +0900

    D - task bajarildi

--- train.js
+++ train.js
@@ -1,45 +1,53 @@
-// C - TASK
-class Shop {
-    non;
-    lagmon;
-    kola;
-
-    constructor(non, lagmon, kola) {
-        this.non = non;
-        this.lagmon = lagmon;
-        this.kola = kola;
-    }
-
-    
-    _time() {
-        return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    }
-
-    qoldiq() {
-        const vaqt = this._time();
-        console.log(`Hozir ${vaqt}da ${this.non}ta non, ${this.lagmon}ta lag'mon va ${this.kola}ta kola mavjud!`);
-    }
-
-    sotish(nomi, soni) {
-        this[nomi] -= soni;
-    }
-
-    qabul(nomi, soni) {
-        this[nomi] += soni;
-    }
+// D - TASK
+function checkContent(a, b) {
+    return a.split("").sort().join("") === b.split("").sort().join("")
 }
 
-// call
-const shop = new Shop(4, 5, 2);
-
-shop.qoldiq();
-
-
-shop.sotish("non", 3);
-shop.qabul("kola", 4);
-shop.qoldiq();
+console.log(checkContent('maktab', 'takbam'));
+console.log(checkContent("mitgroup", "gmtiprou"));
+console.log(checkContent("mitroup", "gmtiprou"));
 
 
+// // C - TASK
+// class Shop {
+//     non;
+//     lagmon;
+//     kola;
+
+//     constructor(non, lagmon, kola) {
+//         this.non = non;
+//         this.lagmon = lagmon;
+//         this.kola = kola;
+//     }
+
+    
+//     _time() {
+//         return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+//     }
+
+//     qoldiq() {
+//         const vaqt = this._time();
+//         console.log(`Hozir ${vaqt}da ${this.non}ta non, ${this.lagmon}ta lag'mon va ${this.kola}ta kola mavjud!`);
+//     }
+
+//     sotish(nomi, soni) {
+//         this[nomi] -= soni;
+//     }
+
+//     qabul(nomi, soni) {
+//         this[nomi] += soni;
+//     }
+// }
+
+// // call
+// const shop = new Shop(4, 5, 2);
+
+// shop.qoldiq();
+
+
+// shop.sotish("non", 3);
+// shop.qabul("kola", 4);
+// shop.qoldiq();
 
 
 
@@ -47,7 +55,9 @@ shop.qoldiq();
 
 
 
-// B - TASK
+
+
+// // B _ TASK
 // function countDigits(str) {
 //      const matches = str.match(/\d/g);
 //      return matches ? matches.length : 0;
